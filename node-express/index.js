a1dbc5a845a8a4471fa5fcfaf02645038fb9800e
Camara Laby Damaro <ldamaro98@gmail.com>
2019-03-18 02:18:49 +0100

    Express Serve Static Files

--- node-express/index.js
+++ node-express/index.js
@@ -1,16 +1,18 @@
 const http = require('http'),
     express = require('express'),
+    morgan = require('morgan'),
     hostname = 'localhost',
     port = 3000,
     app = express();
 
-
-    app.use((req, res, next) => {
-        console.log(req.headers);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<!DOCTYPE html><html><head><title>Express</title></head><body><h1>This is an Express Server</h1></body></html>');
-    });
+    app.use(morgan('dev'))
+        .use(express.static(__dirname + '/public'))
+        .use((req, res, next) => {
+            console.log(req.headers);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<!DOCTYPE html><html><head><title>Express</title></head><body><h1>This is an Express Server</h1></body></html>');
+        });
 
 const server = http.createServer(app);
     server.listen(port, hostname, () => {
